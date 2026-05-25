0237814638d5dc30d93936f658a5762c5380e1fa
ai9361348 <ai9361348@gmail.com>
2026-05-25 19:40:37 +0600

    d [AI 94%]

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Banner from '../components/Home/Banner'
+import Banner from '../components/Home/Banners'
 import Catagory from '../components/Catagory'
 import SmartPhones from '../components/Home/SmartPhones'
 import FeatureProduct from '../components/Home/FeatureProduct'
